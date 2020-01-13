4a1542ec95499f57bbb54ebe77db57c15d3fc32b
lihib94 <lihib94@gmail.com>
2020-01-13 19:53:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationtemp/RequestOfUser.java
@@ -0,0 +1,39 @@
+package com.example.myapplicationtemp;
+
+public class RequestOfUser {
+
+    String userId;
+    String RequestText;
+    long creationTime;
+
+    public RequestOfUser(String uID,String RequestTxt,long cTime) {
+
+        this.userId = uID;
+        this.RequestText = RequestTxt;
+        this.creationTime=cTime;
+
+    }
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getRequestText() {
+        return RequestText;
+    }
+
+    public void setRequestText(String requestText) {
+        RequestText = requestText;
+    }
+
+    public long getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(long creationTime) {
+        this.creationTime = creationTime;
+    }
+}
